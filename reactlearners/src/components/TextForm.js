1fc06d9101021ff8f014111f2d626528b68224f8
Kishor <yoyokunal156@gmail.com>
2023-04-18 13:19:29 +0530

    new

--- reactlearners/src/components/TextForm.js
+++ reactlearners/src/components/TextForm.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter text here");
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+
+  // text = "new text"; //Wrong way to change the state
+  // setText = "new text"; // Correct way to change the state
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div class="mb-3">
-        <textarea class="form-control" id="myBox" rows="8"></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            onChange={handleOnChange}
+            value={text}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
       </div>
-    </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+      </div>
+    </>
   );
 }
