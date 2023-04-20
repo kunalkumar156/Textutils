716e613c69b3732dce0a537fbb2efe6139c0aebe
Kishor <yoyokunal156@gmail.com>
2023-04-20 13:21:30 +0530

    learningdarkmode

--- reactlearners/src/components/TextForm.js
+++ reactlearners/src/components/TextForm.js
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   const handleUpClick = () => {
     // console.log("Uppercase was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
   };
 
+  const handleLoClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleExtraSpaces = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
   const handleOnChange = (event) => {
     // console.log("On change");
     setText(event.target.value);
@@ -28,15 +45,27 @@ export default function TextForm(props) {
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
           Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Delete spaces
         </button>
       </div>
       <div className="container my-3">
         <h1>Your text summary</h1>
         <p>
-          {text.split(" ").length} words and {text.length} characters
+          {text.split(" ").length} words and {text.length} characters.
         </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
     </>
   );
