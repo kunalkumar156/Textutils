65b5b8cd6131416487bdc8e80a0ac9bacc11c09c
Kishor <yoyokunal156@gmail.com>
2023-04-24 19:31:39 +0530

    new

--- reactlearners/src/App.js
+++ reactlearners/src/App.js
@@ -6,14 +6,24 @@ import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
 function App() {
   const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#3D3B3B";
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
   };
   return (
@@ -24,7 +34,7 @@ function App() {
         mode={mode}
         toggleMode={toggleMode}
       />
-      <Alert />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" mode={mode} />
         {/* <About /> */}
--- reactlearners/src/components/Alert.js
+++ reactlearners/src/components/Alert.js
@@ -2,17 +2,20 @@ import React from "react";
 
 export default function Alert(props) {
   return (
-    <div
-      className="alert alert-warning alert-dismissible fade show"
-      role="alert"
-    >
-      {props.alert}
-      <button
-        type="button"
-        className="btn-close"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-      ></button>
-    </div>
+    props.alert && (
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>{props.alert.type}</strong>
+        {props.alert.msg}
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+    )
   );
 }
